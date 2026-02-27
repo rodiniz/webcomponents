17d671716a9b814df1a106f0c86c3dda7dd4094d
Rodrigo Diniz <rodrigo.diniz.contractor@euronext.com>
2026-02-27 16:20:29 +0000

    fix: Remove unused ValidationResult type and update DOM helpers export

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -18,7 +18,7 @@ export { BaseComponent } from '../core/base-component';
 export type { Signal } from '../core/base-component';
 
 export type { ButtonVariant, ButtonSize } from '../shared/components/button';
-export type { InputType, ValidationResult, CustomValidator, ValidationRule } from '../shared/components/input';
+export type { InputType, CustomValidator, ValidationRule } from '../shared/components/input';
 export type { TableColumn, TableRow } from '../shared/components/table';
 export type { SelectOption } from '../shared/components/select';
 export type { TabChangeDetail } from '../shared/components/tabs';
@@ -30,8 +30,8 @@ export { http, HTTPClient } from '../core/http';
 export type { RequestConfig, ResponseConfig, RequestInterceptor, ResponseInterceptor, ErrorInterceptor } from '../core/http';
 
 // DOM Helpers
-export { queryElement, queryElements, queryTable, queryPagination, queryModal, addEventListenerById, getElementById, getFormValues } from '../core/dom-helpers';
-export type { UITableElement, UIPaginationElement, UIModalElement, FormValue, GetFormValuesOptions } from '../core/dom-helpers';
+export { queryElement, queryElements, queryTable, queryPagination, queryModal, addEventListenerById, getElementById, getFormValues, validateForm } from '../core/dom-helpers';
+export type { UITableElement, UIPaginationElement, UIModalElement, FormValue, GetFormValuesOptions, ValidationResult } from '../core/dom-helpers';
 
 // Router
 export { createRouter } from '../core/router';
